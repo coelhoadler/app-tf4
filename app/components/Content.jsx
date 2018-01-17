67abc3e99f3b2efc509ec61553c5b083689240f6
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2018-01-17 21:43:26 -0200

    Adding axios module to make requests.

--- app/components/Content.jsx
+++ app/components/Content.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 export default class Content extends Component {
 
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        const URL = "https://app-t4f.firebaseio.com/.json";
+        axios.get(URL).then(response => {
+            console.log(response);
+        }, error => {
+
+        });
     }
 
     render() {
